c4435ba30eeca225ba3d56ee926040db69903427
Alex Hadley <alexhad6@users.noreply.github.com>
2023-11-08 20:22:34 -0800

    Fix @jupyter/collaboration licensing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const {
+  WPPlugin: { JSONLicenseWebpackPlugin },
+} = require("@jupyterlab/builder");
 
 module.exports = {
   resolve: {
@@ -6,4 +9,11 @@ module.exports = {
       "@": path.resolve(__dirname, "lib"),
     },
   },
+  plugins: [
+    new JSONLicenseWebpackPlugin({
+      licenseFileOverrides: {
+        "@jupyter/collaboration": "LICENSE-downloaded",
+      },
+    }),
+  ],
 };
